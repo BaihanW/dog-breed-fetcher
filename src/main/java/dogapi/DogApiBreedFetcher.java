df096ec26045cb35c78ba6e193240cc8d18caf14
BaihanW <baihan.wu@mail.utoronto.ca>
2025-10-11 23:39:11 -0400

    haha

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -24,12 +24,33 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
-        // TODO Task 1: Complete this method based on its provided documentation
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
+        //  Task 1: Complete this method based on its provided documentation
         //      and the documentation for the dog.ceo API. You may find it helpful
         //      to refer to the examples of using OkHttpClient from the last lab,
         //      as well as the code for parsing JSON responses.
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+
+        Request request = new Request.Builder()
+                .url("https://dog.ceo/api/breeds/list/all")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            String responseBody = response.body().string();
+            JSONObject json = new JSONObject(responseBody);
+            JSONObject message = json.getJSONObject("message");
+            if (!message.has(breed)) {
+                throw new BreedNotFoundException("Breed not found: " + breed);
+            }
+            JSONArray subBreedArray = message.getJSONArray(breed);
+            List<String> subBreeds = new ArrayList<>();
+            for (int i = 0; i < subBreedArray.length(); i++) {
+                subBreeds.add(subBreedArray.getString(i));
+            }
+            // return statement included so that the starter code can compile and run.
+            return subBreeds;
+        }catch (IOException e) {
+            throw new BreedNotFoundException("Network error: " + e.getMessage());
+        }
+
     }
 }
